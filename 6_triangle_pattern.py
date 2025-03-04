deda852073c47debf795803790d9c8d60e9c1fdd
MeenatchisundaramS <smeenatchisundaram14@gmail.com>
2025-03-04 10:35:26 +0530

    file deleted

--- 6_triangle_pattern.py
+++
@@ -1,9 +0,0 @@
-def triangle_pattern(num):
-	for i in range(num):
-		print(" " * (num-i) + "*" * (2*i-1))
-
-	for i in range(num,0,-1):
-		print(" "*(num-i) + "*" * (2*i-1))
-
-user_num=int(input("Enter the number : "))
-triangle_pattern(num=user_num)
