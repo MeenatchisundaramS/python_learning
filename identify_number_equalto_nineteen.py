c18da90ee2376b6d15aad609b621fe1f47c1865b
MeenatchisundaramS <smeenatchisundaram14@gmail.com>
2025-03-16 19:12:49 +0530

    To identify the number is equal to nineteen

---
+++ identify_number_equalto_nineteen.py
@@ -0,0 +1,10 @@
+# get the number from user and identify if the number is equal to nineteen
+
+def check(num):
+	if num==90:
+		print(f"The number {num} is equal to nineteen")
+	else:
+		print(f"The number {num} is not equal to nineteen")
+
+user_num=int(input("Enter the number : "))
+check(num=user_num)
