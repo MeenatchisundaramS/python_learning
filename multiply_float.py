171581b9fa31ec4256c9f6b38e3281e1e6ace6cf
MeenatchisundaramS <smeenatchisundaram14@gmail.com>
2025-03-16 19:18:49 +0530

    To multiply the two float number

---
+++ multiply_float.py
@@ -0,0 +1,9 @@
+#get two decimal  number, multiplication them and print it on console
+
+def multiply(num1,num2):
+	num=num1*num2
+	print(f"The multiplication of two number is : {num}")
+
+user_num1=float(input("Enter the number 1 : "))
+user_num2=float(input("Enter the number 2 : "))
+multiply(num1=user_num1,num2=user_num2)
