d309c0031423bf09a403c44d2f629452acba4b90
MeenatchisundaramS <smeenatchisundaram14@gmail.com>
2025-03-17 15:11:42 +0530

    To get two integer and add

---
+++ add_twointeger.py
@@ -0,0 +1,9 @@
+# get two integer number, add them and print it on console
+
+def add(num1,num2):
+	addition=num1+num2
+	print(f"The addition of two number is : {addition}")
+
+user_num1=int(input("Enter the number 1 : "))
+user_num2=int(input("Enter the number 2 : "))
+add(num1=user_num1,num2=user_num2)
