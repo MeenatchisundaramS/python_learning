d71f9aef27d41cc0edd19c5e4bb1cf336dcfe35e
MeenatchisundaramS <smeenatchisundaram14@gmail.com>
2025-03-17 15:12:58 +0530

    To get float number and divide them

---
+++ divide_twofloat.py
@@ -0,0 +1,9 @@
+# get two decimal number, division them and print it on console
+
+def division(num1,num2):
+	division=num1/num2
+	print(f"The division of two number is : {division}")
+
+user_num1=float(input("Enter the number 1 : "))
+user_num2=float(input("Enter the number 2 : "))
+division(num1=user_num1,num2=user_num2)
